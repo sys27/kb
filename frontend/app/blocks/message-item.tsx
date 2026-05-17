8a8cfafc045b66129be1de1aac436ffe3eb9afc1
Dmytro Kyshchenko <sys2712@gmail.com>
2026-05-17 12:19:23 +0300

    Add marked and hightlight.js to format messages.

--- frontend/app/blocks/message-item.tsx
+++ frontend/app/blocks/message-item.tsx
@@ -1,4 +1,7 @@
+import DOMPurify from 'dompurify';
+import hljs from 'highlight.js';
 import { Copy } from 'lucide-react';
+import { Marked, Renderer } from 'marked';
 import { Badge } from '~/components/ui/badge';
 import { Button } from '~/components/ui/button';
 import {
@@ -17,6 +20,16 @@ import {
     type Message,
 } from '~/services/messages';
 
+const renderer = new Renderer();
+renderer.code = function ({ text, lang }): string {
+    let language = hljs.getLanguage(lang || '') ? lang! : 'plaintext';
+    let highlighted = hljs.highlight(text, { language }).value;
+
+    return `<pre class="not-prose whitespace-pre-wrap wrap-break-words overflow-x-auto text-sm"><code class="hljs language-${language}">${highlighted}</code></pre>`;
+};
+
+const marked = new Marked({ renderer });
+
 export default function MessageItem({ message }: { message: Message }) {
     let handleCopy = async () => {
         await navigator.clipboard.writeText(message.text);
@@ -26,6 +39,7 @@ export default function MessageItem({ message }: { message: Message }) {
         message.messageTypeId == MessageType.userRequestId;
     let role = getMessageRoleName(message);
     let kind = getMessageKindName(message);
+    let md = DOMPurify.sanitize(marked.parse(message.text) as string);
 
     return (
         <Collapsible defaultOpen={isOpen}>
@@ -44,7 +58,12 @@ export default function MessageItem({ message }: { message: Message }) {
                 </CardHeader>
 
                 <CollapsibleContent className="flex flex-col gap-4">
-                    <CardContent className="whitespace-pre-wrap">{message.text}</CardContent>
+                    <CardContent>
+                        <article
+                            className="prose max-w-none dark:prose-invert"
+                            dangerouslySetInnerHTML={{ __html: md }}
+                        />
+                    </CardContent>
 
                     <CardFooter className="flex flex-row justify-between px-2 py-0">
                         <Button
